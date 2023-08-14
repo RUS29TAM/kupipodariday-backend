70c3f6ae4f63df64f819a4b673ebdf70bea83b85
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-08-14 17:32:59 +0300

    chore: add methods for users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,24 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'Это действие добавляет нового пользователя';
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(user);
   }
 
-  findAll() {
-    return `Это действие возвращает всех пользователей`;
+  // findAll() {
+  //   return `Это действие возвращает всех пользователей`;
+  // }
+
+  async findOne(id: number) {
+    const user = await this.usersRepository.findOneBy({ id });
+    return user;
   }
 
-  findOne(id: number) {
-    return `Это действие возвращает #${id} пользователя`;
+  async findeByUserName(username: string) {
+    const user = await this.usersRepository.findOneBy({ username });
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `Это действие обновляет #${id} пользователя`;
-  }
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `Это действие обновляет #${id} пользователя`;
+  // }
 
   remove(id: number) {
     return `Это действие удаляет #${id} пользователя`;
