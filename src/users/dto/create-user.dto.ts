8d8dc4fd8094afdfae81b49eee73497dc110faa1
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-08-14 11:18:47 +0300

    chore: add create-user.dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,34 @@
-export class CreateUserDto {}
+import {
+  IsString,
+  IsEmail,
+  MaxLength,
+  MinLength,
+  IsNotEmpty,
+  IsUrl,
+  IsOptional,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MaxLength(64)
+  @MinLength(1)
+  @IsNotEmpty()
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @MinLength(2)
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @MaxLength(200)
+  @IsOptional()
+  about?: string;
+
+  @IsUrl()
+  @IsOptional()
+  avatar?: string;
+}
