dce8c6c43c1cdf597bbaa973d3ed7f021baf536e
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-08-15 16:42:35 +0300

    chore: draft configuring the Auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { LocalGuard } from './guards/local.guard';
+import { JwtGuard } from './guards/jwt.guard';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
+import { UsersModule } from '../users/users.module';
+import { HashModule } from '../hash/hash.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import jwtConfig from '../config/jwt.config';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    UsersModule,
+    HashModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: jwtConfig,
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalGuard, JwtGuard],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
+import { Offer } from '../entities/offer.entity';
+import { Wish } from '../entities/wish.entity';
+import { Wishlist } from '../entities/wishlist.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Offer, Wish, Wishlist])],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
