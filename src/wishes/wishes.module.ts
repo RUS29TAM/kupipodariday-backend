eb45c1a263b5fd94b1b436a85b16d1de14821a64
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-08-16 18:07:52 +0300

    feat: configure wish creation DTO

--- src/wishes/wishes.module.ts
+++ src/wishes/wishes.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { WishesService } from './wishes.service';
 import { WishesController } from './wishes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
+import { Offer } from '../entities/offer.entity';
+import { Wish } from '../entities/wish.entity';
+import { Wishlist } from '../entities/wishlist.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Offer, Wish, Wishlist])],
   controllers: [WishesController],
   providers: [WishesService],
+  exports: [WishesService],
 })
 export class WishesModule {}
