51d6593bf7fc5cd55cb8702d6181365c03a90cf8
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-08-16 16:58:54 +0300

    chore: add an interceptor blank

---
+++ src/interceptors/password-user.interceptor.ts
@@ -0,0 +1,17 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class PasswordUserInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return;
+  }
+}
